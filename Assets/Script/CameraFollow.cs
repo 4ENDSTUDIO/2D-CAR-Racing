d9d0ed9fb033876c5a37c07e0be96ffad417365a
Rizky Hendra Saputra <101460749+4ENDSTUDIO@users.noreply.github.com>
2022-05-20 16:15:16 +0700

    web gl

--- Assets/Script/CameraFollow.cs
+++ Assets/Script/CameraFollow.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Transform Target;
+    public Vector3 Offset;
+    public float smoothness;
     void Start()
     {
         
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        Vector3 delayedPos = Vector3.Lerp(transform.position, Target.position, smoothness);
+        transform.position = delayedPos + Offset;
     }
+
+    
 }
---
+++ Assets/Script/CameraRotation.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotation : MonoBehaviour
+{
+    public float[] RotZ;
+    int triggerCount = 0;
+
+    Camera Cam;
+
+    bool CanRotate;
+
+    public float rotSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (CanRotate)
+        {
+            Quaternion camRot = Cam.transform.rotation;
+            Quaternion desRot = Quaternion.Euler(0, 0, RotZ[triggerCount - 1]);
+
+            Cam.transform.rotation = Quaternion.Lerp(camRot, desRot, Time.deltaTime * rotSpeed);
+
+            if (camRot.z == desRot.z)
+            {
+                CanRotate = false;
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Hit");
+        if (triggerCount == 4)
+            triggerCount = 0;
+
+        triggerCount += 1;
+
+        CanRotate = true;
+
+    }
+}
